396ce8ebacf53e4c77eb25ca0b6b9419032bd660
styxpilled <styxpilled.dev@gmail.com>
2024-10-03 06:21:30 +0200

    feat: Zarathustra font preview

---
+++ src/routes/fonts/Zarathustra/Preview.svelte
@@ -0,0 +1,37 @@
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/Zarathustra/index.css" />
+</svelte:head>
+
+<div class="preview noise">
+	<h3>Zarathustra</h3>
+	<p>
+		<a href="https://github.com/lorene-cn/zarathustra-typeface"><em>Zarathustra</em></a> by
+		<a href="http://lorene-picard.com/"><em>Lorène Picard</em></a> is a revival of a typeface designed
+		by Belgian painter Georges Lemmen at the beggining of the 20th century.
+	</p>
+</div>
+
+<style>
+	.preview {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		font-family: 'Zarathustra', 'Wingdings';
+		color: #58180d;
+
+		--background: white;
+		border-radius: 1rem;
+		border: 2px solid #f5f5f5;
+		font-size: 1.5rem;
+
+		& > * {
+			max-width: 50dvw;
+			text-align: center;
+		}
+	}
+
+	h3 {
+		font-size: 6rem;
+	}
+</style>
