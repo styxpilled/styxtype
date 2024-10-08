9ba6382ec16af8ec2624d62c824e68c48eba845a
styxpilled <styxpilled.dev@gmail.com>
2024-10-08 03:36:26 +0200

    feat: josefin sans and merriweather previews

---
+++ src/routes/fonts/Josefin Sans/Preview.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import '@fontsource-variable/josefin-sans';
+	import '@fontsource-variable/josefin-sans/wght-italic.css';
+	let weight = $state(400);
+	let min = 100;
+	let max = 900;
+
+	const colors = ['#ec1c00', '#f5b500', '#006021', '#ab0d44', '#435b9e'];
+</script>
+
+<div class="preview" style:font-weight={weight}>
+	<h3>
+		Josefin Sans
+		<!-- {#each 'Josefin Sans'.split('') as char, i}
+			<span style="color: {colors[i % colors.length]}">{char}</span>
+		{/each} -->
+	</h3>
+	<p>
+		<em><a href="https://indestructibletype.com/Jost">Josefin Sans</a></em> by
+		<em><a href="https://indestructibletype.com">Santiago Orozco</a></em> is inspired by geometric sans
+		serifs of the 1920 like Futura.
+	</p>
+	<label>
+		<span class="label">Weight:</span><span>{weight}</span>
+		<input type="range" bind:value={weight} {min} {max} />
+	</label>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Josefin Sans Variable', 'Wingdings';
+		color: whitesmoke;
+		background: repeating-linear-gradient(
+				45deg,
+				#717ed7,
+				#717ed7 8px,
+				transparent 8px,
+				transparent 100px
+			),
+			repeating-linear-gradient(-45deg, #606dbc, #606dbc 8px, transparent 8px, transparent 100px),
+			repeating-linear-gradient(
+				45deg,
+				transparent,
+				transparent 16px,
+				#262224 16px,
+				#262224 24px,
+				transparent 24px,
+				transparent 100px
+			),
+			repeating-linear-gradient(
+				-45deg,
+				transparent,
+				transparent 16px,
+				#332e31 16px,
+				#332e31 24px,
+				transparent 24px,
+				transparent 100px
+			),
+			linear-gradient(to bottom, #c474b5, #c474b5);
+		/* background-attachment: fixed; */
+		min-height: 25rem;
+	}
+
+	h3 {
+		letter-spacing: -0.5rem;
+	}
+</style>
---
+++ src/routes/fonts/Merriweather/Preview.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import '@fontsource-variable/shantell-sans/full.css';
+
+	import '@fontsource/merriweather/300.css';
+	import '@fontsource/merriweather/400.css';
+	import '@fontsource/merriweather/700.css';
+	import '@fontsource/merriweather/900.css';
+	import '@fontsource/merriweather/300-italic.css';
+	import '@fontsource/merriweather/400-italic.css';
+	import '@fontsource/merriweather/700-italic.css';
+	import '@fontsource/merriweather/900-italic.css';
+
+	// Sorkin Type, Eben Sorkin
+	// https://github.com/SorkinType/Merriweather/
+
+	let weight = $state(400);
+	let min = 300;
+	let max = 900;
+</script>
+
+<div class="preview noise-light" style:font-weight={weight}>
+	<h3>Merriweather</h3>
+	<p>
+		Merriweather was designed to be a text face that is pleasant to read on screens. It features a
+		very large x height, slightly condensed letterforms, a mild diagonal stress, sturdy serifs and
+		open forms.
+	</p>
+	<label>
+		<span class="label">Weight:</span><span>{weight}</span>
+		<input type="range" bind:value={weight} {min} {max} step="100" />
+	</label>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Merriweather', 'Wingdings';
+		color: #ecf0da;
+
+		--background: #557663;
+	}
+</style>
