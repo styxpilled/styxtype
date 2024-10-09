1bef4fff4913b37aaf9de849cf57bf417da84534
styxpilled <styxpilled.dev@gmail.com>
2024-10-10 00:18:51 +0200

    feat: Pilowlava and Rubik previews

---
+++ src/routes/fonts/Pilowlava/Preview.svelte
@@ -0,0 +1,103 @@
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/Pilowlava/index.css" />
+</svelte:head>
+
+<!-- https://velvetyne.fr/fonts/pilowlava/ -->
+
+<div class="preview">
+	<div class="bg">
+		<h3>Pilowlava</h3>
+		<p>
+			Pilowlava seeks a balance between viscous energy and controlled geometry. Inspired by the flow
+			of lava, the typeface evokes the psychedelic albums of the late 60's and early 70's. The
+			structures of its glyphs are mostly derived from hand-written dynamics, that feed from both
+			calligraphic and graffiti references.
+		</p>
+	</div>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Pilowlava Atome', 'Wingdings';
+		color: #ef5924;
+		display: flex;
+		padding: 0;
+		--size: 128px;
+
+		--c1: #ed1d78;
+		--c2: #f9ae39;
+		--c3: #ef5924;
+		--c4: #642c91;
+
+		background:
+			radial-gradient(
+				var(--size) at 100% 0,
+				var(--c4) 6.25%,
+				var(--c2) 6.3% 18.75%,
+				var(--c1) 18.8% 31.25%,
+				var(--c2) 31.3% 43.75%,
+				var(--c1) 43.8% 56.25%,
+				var(--c2) 56.3% 68.75%,
+				#0000 0
+			),
+			radial-gradient(
+				var(--size) at 0 0,
+				var(--c4) 6.25%,
+				var(--c2) 6.3% 18.75%,
+				var(--c1) 18.8% 31.25%,
+				var(--c2) 31.3% 43.75%,
+				var(--c1) 43.8% 56.25%,
+				var(--c2) 56.3% 68.75%,
+				#0000 0
+			),
+			radial-gradient(
+				var(--size) at 0 100%,
+				var(--c4) 6.25%,
+				var(--c2) 6.3% 18.75%,
+				var(--c1) 18.8% 31.25%,
+				var(--c2) 31.3% 43.75%,
+				var(--c1) 43.8% 56.25%,
+				var(--c2) 56.3% 68.75%,
+				#0000 0
+			),
+			radial-gradient(
+					var(--size) at 100% 100%,
+					var(--c4) 6.25%,
+					var(--c2) 6.3% 18.75%,
+					var(--c1) 18.8% 31.25%,
+					var(--c2) 31.3% 43.75%,
+					var(--c4) 43.8% 56.25%,
+					var(--c2) 56.3% 68.75%,
+					#0000 0
+				)
+				var(--c1);
+		background-size: var(--size) var(--size);
+		background-color: var(--c1);
+		box-shadow: 0 0 0.5rem 0.5rem #f1f1f1 inset;
+	}
+
+	.bg {
+		/* background-color: red; */
+		backdrop-filter: blur(2px);
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		/* : center; */
+		padding: 1rem;
+		height: 100%;
+		width: 100%;
+	}
+
+	h3 {
+		text-shadow: 2px 2px 1px black;
+	}
+
+	p {
+		font-family: var(--font-ui);
+		max-width: 50dvw;
+		color: black;
+		border-radius: 1rem;
+		padding: 1rem;
+	}
+</style>
---
+++ src/routes/fonts/Rubik/Preview.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { variationStyle } from '$lib';
+	import type { CSSSlider } from '$lib/types';
+
+	// https://hfs-studio.com/
+	// https://fonts.google.com/specimen/Rubik
+
+	let sliders = $state<CSSSlider[]>([
+		{
+			label: 'Weight',
+			cssProperty: 'wght',
+			min: 300,
+			max: 900,
+			value: 500
+		}
+	]);
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/Rubik/index.css" />
+</svelte:head>
+
+<div class="preview" style={variationStyle(sliders)}>
+	<h3>RUBIK</h3>
+	<p>
+		Rubik is a sans serif font family with slightly rounded corners designed by <a
+			href="https://hfs-studio.com/"
+		>
+			Philipp Hubert and Sebastian Fischer
+		</a>. Rubik is a 5 weight family with Roman and Italic styles, that accompanies Rubik Mono One,
+		a monospaced variation of the Black roman design.
+	</p>
+	{#each sliders as slider}
+		<label>
+			<span class="label">{slider.label}</span><span
+				style:width={slider.max.toString().length +
+					(slider.step || 5).toString().length +
+					(slider.min < 0 ? 1 : 0) +
+					'ch'}>{slider.value}</span
+			>
+			<input type="range" bind:value={slider.value} min={slider.min} max={slider.max} />
+		</label>
+	{/each}
+</div>
+
+<style>
+	.preview {
+		font-family: 'Rubik', 'Wingdings';
+		color: #ffffff;
+
+		--background: #e83956;
+		background-color: #e83956;
+	}
+
+	label {
+		width: 50rem;
+	}
+</style>
