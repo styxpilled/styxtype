43cdd40f269e48fe5233eeebede67a3b7873c545
styxpilled <styxpilled.dev@gmail.com>
2024-10-10 05:25:13 +0200

    feat: Patriot and Westgate previews

---
+++ src/routes/fonts/Patriot/Preview.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let weight = $state(600);
+	const min = 100;
+	const max = 700;
+	const step = 100;
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/Patriot/index.css" />
+</svelte:head>
+
+<div class="preview" style:font-weight={weight}>
+	<h3>Patriot</h3>
+	<p>
+		Patriot is a display sans serif typeface with a great quantity of distinctive features, from
+		irregular crossbars to abrupt diagonal strokes. It also demonstrates its industrialist nature
+		with a uniformity in weight, and no flourishes nor distinctions between thick and thin lines.
+		Traces of Art Deco, Bauhaus and hints of Constructivism are preserved in the digital typeface,
+		promising a progressive and influential voice when used in proper manners.
+	</p>
+	<label>
+		<span class="label">Weight:</span><span>{weight}</span>
+		<input type="range" bind:value={weight} {min} {max} {step} />
+	</label>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Patriot', 'Wingdings';
+		color: #ffda40;
+		background-color: #e20d0f;
+	}
+</style>
---
+++ src/routes/fonts/Westgate/Preview.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	let weight = $state(600);
+	const min = 100;
+	const max = 500;
+	const step = 100;
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/Westgate/index.css" />
+</svelte:head>
+
+<!-- https://republi.sh/#westgate/info -->
+
+<div class="preview" style:font-weight={weight}>
+	<h3>Westgate</h3>
+	<p>
+		Westgate was designed based on the vernacular concrete typography on the North, East and West
+		gates of Saigon's iconic Ben Thanh Market. The typeface carries an elegant condensed form
+		inspired by the Art Deco style of the early 20th century French colonial era. It features
+		elongated letter heights, combined with geometric lines and forms. The most charming feature is
+		the Vietnamese diacritics which are harmoniously integrated into the letterforms, thus
+		compensating for the typographic challenges of the Vietnamese language system while not
+		compromising legibility.
+	</p>
+	<label>
+		<span class="label">Weight:</span><span>{weight}</span>
+		<input type="range" bind:value={weight} {min} {max} {step} />
+	</label>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Westgate', 'Wingdings';
+		color: #0d9e8f;
+		background-color: #ffd243;
+	}
+
+	p {
+		font-family: var(--font-ui);
+	}
+</style>
