742a1171c56a18a21aebdcb3e2f358c80b3e340d
styxpilled <styxpilled.dev@gmail.com>
2024-10-11 07:11:25 +0200

    feat: Gotico Antiqua Durandus preview

---
+++ src/routes/fonts/Gotico Antiqua Durandus/Preview.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/GoticoAntiquaDurandus/index.css" />
+</svelte:head>
+
+<!-- https://github.com/anrt-type/GoticoAntiqua -->
+
+<div class="preview noise">
+	<h3>Gotico Antiqua Durandus</h3>
+	<p>
+		Gotico-Antiqua first used in Mainz by Peter Schoffer & Johann Fust for Guillaume Durand’s
+		Rationale Divinorum Officorum in the XIV century.
+	</p>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Gotico Antiqua Durandus', 'Wingdings';
+		color: black;
+
+		--background: rgb(235, 221, 170);
+	}
+
+	h3 {
+		line-height: 1;
+	}
+</style>
