2fcaf007368f7f835929df58abcf7dee2bfa26f9
styxpilled <styxpilled.dev@gmail.com>
2024-10-12 08:06:49 +0200

    feat: Beon preview

---
+++ src/routes/fonts/Beon/Preview.svelte
@@ -0,0 +1,51 @@
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/Beon/index.css" />
+</svelte:head>
+
+<div class="preview">
+	<h3>«Beon»</h3>
+	<p>Beon by Bastien Sozeau is a single-weight stencil typeface imitating neon tube signs.</p>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Beon', 'Wingdings';
+		color: whitesmoke;
+		background-color: black;
+	}
+
+	h3 {
+		animation: flicker 5s infinite alternate;
+	}
+
+	@keyframes flicker {
+		0%,
+		18%,
+		22%,
+		25%,
+		53%,
+		57%,
+		100% {
+			text-shadow:
+				0 0 4px #fff,
+				0 0 11px #fff,
+				0 0 19px #fff,
+				0 0 40px #0fa,
+				0 0 80px #0fa,
+				0 0 90px #0fa,
+				0 0 100px #0fa,
+				0 0 150px #0fa;
+		}
+		20%,
+		24%,
+		55% {
+			text-shadow: none;
+		}
+	}
+
+	@media (prefers-reduced-motion: reduce) {
+		h3 {
+			animation: none;
+		}
+	}
+</style>
