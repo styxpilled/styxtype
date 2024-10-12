64d6ff29d662327fc9ae94c51fe64df8809a3858
styxpilled <styxpilled.dev@gmail.com>
2024-10-12 07:26:42 +0200

    feat: twisty screensaver

--- src/components/Screensaver.svelte
+++ src/components/Screensaver.svelte
@@ -20,16 +20,13 @@
 />
 
 <div class="screensaver col space" class:showing>
-	<div class="marquee">
-		{#each { length: 5 } as _, i}
-			<p class="marquee-content">styxtype - a libre font collection</p>
-		{/each}
-	</div>
-	<div class="marquee">
-		{#each { length: 5 } as _, i}
-			<p class="marquee-content">styxtype - a libre font collection</p>
-		{/each}
-	</div>
+	{#each { length: 3 } as _, i}
+		<p class="marquee-content" style="--delay: {i * 3000}ms;">
+			{#each 'styxtype - a libre font collection'.split('') as char, i}
+				<span style="--index: {i};">{char}</span>
+			{/each}
+		</p>
+	{/each}
 </div>
 
 <style>
@@ -37,60 +34,53 @@
 		position: fixed;
 		background-color: rgba(0, 0, 0, 0.13);
 		width: 100dvw;
-		height: 115dvh;
-		top: -5dvh;
+		height: 100dvh;
 		z-index: 255;
 		pointer-events: none;
-		opacity: 0;
-
-		transition:
-			opacity 250ms linear,
-			height 250ms linear,
-			top 250ms linear;
+		top: 0;
+		display: none;
+		background-color: rgba(0, 0, 0, 0.4);
+		color: whitesmoke;
 	}
 
 	.screensaver.showing {
-		opacity: 1;
-		height: 100dvh;
-		top: 0;
-	}
-
-	.marquee {
-		--gap: 20vw;
-		position: relative;
-		display: flex;
-		overflow: hidden;
-		user-select: none;
-		gap: var(--gap);
-		font-size: 2rem;
-		color: whitesmoke;
-		background-color: rgba(0, 0, 0, 0.4);
-	}
-
-	.marquee-content {
-		flex-shrink: 0;
-		display: flex;
-		justify-content: space-around;
-		gap: var(--gap);
-	}
-
-	@keyframes scroll {
-		from {
-			transform: translateX(0);
+		display: block;
+		& > p > span {
+			animation-play-state: running;
 		}
-		to {
-			transform: translateX(calc(-100% - var(--gap)));
+	}
+
+	span {
+		position: absolute;
+		top: -15%;
+		left: 0%;
+		font-size: 2rem;
+		/* Optimization hacks */
+		transform: translate3d(0, 0, 0);
+		backface-visibility: hidden;
+		perspective: 1000;
+		will-change: offset-distance;
+		/*  */
+		--offset: calc(var(--index) * 1rem);
+		offset-distance: calc(var(--offset) - 25dvw);
+		offset-path: path(
+			'M.36,513.53s259.11,275,615.37-160.06c275.98-337.02,826.06-205.57,594.65,10.7-99.61,93.09-449.38,172.76-510.99,337.82-77.62,207.96,88.97,318.26,296.25,318.26,288.48,0,291.07-398.68,790.42-388.11,209.12,4.43,376.14,133.43,376.14,133.43'
+		);
+		animation-play-state: paused;
+		animation: move 10s infinite forwards var(--delay, 0ms);
+	}
+
+	@keyframes move {
+		100% {
+			offset-distance: calc(var(--offset) + 200dvw);
 		}
 	}
 
 	/* Pause animation when reduced-motion is set */
-	@media (prefers-reduced-motion: reduce) {
-		.marquee-content {
+	@media (prefers-reduced-motion: reduce) or (update: slow) {
+		span {
+			offset-distance: calc(var(--offset) + 50dvw);
 			animation-play-state: paused !important;
 		}
-	}
-
-	.marquee-content {
-		animation: scroll 3s linear infinite;
 	}
 </style>
