4483e55cfa87bf4d6bcf7eabe86387910ca98c85
styxpilled <styxpilled.dev@gmail.com>
2024-10-19 17:40:06 +0200

    feat: Lilex previews

---
+++ src/routes/fonts/Lilex/Preview.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { formatDescription } from '$lib';
+	import { font } from './data';
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/Lilex/index.css" />
+</svelte:head>
+
+<div class="preview">
+	<p class="spacer">&lt===&lt&lt===&lt&lt=&gt&gt===&gt&gt===&gt</p>
+	<h3>&lt/Lilex/&gt</h3>
+	<p class="spacer">&lt===&lt&lt===&lt&lt=&gt&gt===&gt&gt===&gt</p>
+	<p>
+		{@html formatDescription(font)}
+	</p>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Lilex', 'Wingdings';
+		color: #e6e5ff;
+		--color: #e6e5ff;
+		background-color: #0500ff;
+		--background: #0500ff;
+	}
+
+	h3 {
+		line-height: 1;
+	}
+
+	.spacer {
+		color: #9da1a4;
+		font-size: 3rem;
+	}
+</style>
---
+++ src/routes/fonts/Lilex/data.ts
@@ -0,0 +1,21 @@
+import type { Font } from '$lib/types';
+
+export const font: Font = {
+	title: 'Lilex',
+	href: 'https://lilex.myrt.co/',
+	author: {
+		name: '',
+		href: ''
+	},
+	category: 'monospace',
+	characters: '',
+	description:
+		'%TITLE% is an extended font on top of IBM Plex Mono designed for developers. It contains ligatures, special characters (e.g. PowerLine), Greek and exists in a variable format.',
+	license: 'SIL',
+	links: [
+		{
+			href: 'https://github.com/mishamyrt/Lilex',
+			label: 'GitHub'
+		}
+	]
+};
