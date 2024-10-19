27e2f7f4bbf6246c9156cb6e4e8872db52df4316
styxpilled <styxpilled.dev@gmail.com>
2024-10-19 17:40:50 +0200

    feat: Edge Display preview

---
+++ src/routes/fonts/Edge Display/Preview.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { formatDescription } from '$lib';
+	import { font } from './data';
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/font-source/EdgeDisplay/index.css" />
+</svelte:head>
+
+<div class="preview">
+	<h3>{font.title}</h3>
+	<p>
+		{@html formatDescription(font)}
+	</p>
+</div>
+
+<style>
+	.preview {
+		font-family: 'Edge Display', 'Wingdings';
+		color: #f2f2f2;
+		--color: #f2f2f2;
+		background-color: #303030;
+		--background: #303030;
+	}
+
+	h3,
+	.preview :global(a:hover) {
+		color: #ffd83b;
+		--color: #ffd83b;
+	}
+
+	h3 {
+		text-decoration: underline;
+		text-decoration-color: #f2f2f2;
+	}
+</style>
---
+++ src/routes/fonts/Edge Display/data.ts
@@ -0,0 +1,15 @@
+import type { Font } from '$lib/types';
+
+export const font: Font = {
+	title: 'Edge Display',
+	author: {
+		name: 'Grzegorz Samson',
+		href: 'https://grzegorzsamson.pl'
+	},
+	href: 'https://grzegorzsamson.pl/project/edge-display',
+	category: 'sans-serif',
+	characters: '',
+	description: `%TITLE% by %AUTHOR% is sans-serif typeface best suited for large text. It was inspired by music posters, band logos and concert art.`,
+	license: 'SIL',
+	links: []
+};
